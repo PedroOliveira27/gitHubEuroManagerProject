0280f76af1530c377705f179e51d6345e7d7568f
PedroOliveira27 <pedrofg.oliveira@hotmail.com>
2021-06-23 18:07:02 +0100

    Added class Game

---
+++ src/Game/Game.java
@@ -0,0 +1,34 @@
+package Game;
+
+import academy.mindswap.teams.Team;
+import academy.mindswap.teams.TeamHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    List<Team> createGroup = new ArrayList<>();
+
+
+    public void ChooseTeam (String teamName){
+        if (teamName.equals("Portugal")){
+            Team team = TeamHandler.createTeam(new Team("Portugal"),TeamHandler.portugal);
+            createGroup.add(team);
+        }
+        if (teamName.equals("Germany")){
+            Team team = TeamHandler.createTeam(new Team("Germany"),TeamHandler.germany);
+            createGroup.add(team);
+        }
+        if (teamName.equals("France")){
+            Team team = TeamHandler.createTeam(new Team("France"),TeamHandler.france);
+            createGroup.add(team);
+        }
+        if (teamName.equals("Hungry")){
+            Team team = TeamHandler.createTeam(new Team("Hungary"),TeamHandler.hungary);
+            createGroup.add(team);
+        }
+
+    }
+
+
+}
