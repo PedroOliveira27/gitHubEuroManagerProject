f3f7109068c905ceb753a0fb3e674a293ad2eb8d
DavidCortT <davidcorteztrind@gmail.com>
2021-06-23 11:26:58 +0100

    added Training - first version

---
+++ src/academy/mindswap/training/Training.java
@@ -0,0 +1,38 @@
+package academy.mindswap.training;
+
+import academy.mindswap.players.Players;
+
+
+public class Training {
+    Players player;
+    Players age;
+
+
+    public void developPlayers(Players player) {
+
+        System.out.println("Player x, picked " + player.getName() + "for a training session");
+        int playerDevelopment = 0;
+
+
+        if (player.getAge() <= 21) {
+            playerDevelopment = (int) (Math.random() * 5);
+            player.setOverall(player.getOverall() + playerDevelopment);
+            System.out.println(player.getName() + "improve is overall to " + player.getOverall());
+        } else if (player.getAge() > 21 && player.getAge() <= 25) {
+            playerDevelopment = (int) (Math.random() * 3);
+            player.setOverall(player.getOverall() + playerDevelopment);
+            System.out.println(player.getName() + "improve is overall to " + player.getOverall());
+
+        } else {
+            playerDevelopment = (int) (Math.random() * 1);
+            player.setOverall(playerDevelopment);
+            System.out.println(player.getName() + "improve is overall to " + player.getOverall());
+
+
+        }
+
+
+    }
+
+
+}
