7dbb2f4880b2e6e771e925999df694566fc1687e
silva-angelo <silva.angelo@hotmail.com>
2021-06-23 11:11:51 +0100

    printPlayerList() added to print players

--- src/academy/mindswap/teams/Team.java
+++ src/academy/mindswap/teams/Team.java
@@ -7,13 +7,20 @@ import java.util.Set;
 
 public class Team {
 	private Set<Players> team;
-	private int teamOverall;
 
 	public Team() {
 		team = new HashSet<>();
 	}
 
-	private int getOverall() {
+	public void printPlayerList(){
+		for (Players p : team) {
+			System.out.print(p.getName() + " | " +
+					p.getOverall + " | " +
+					p.getPosition);
+		}
+	}
+
+	public int getOverall() {
 		int overall = 0;
 		for (Players p : team) {
 			overall += p.getOverall();
