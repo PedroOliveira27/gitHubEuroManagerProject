d5b1d0cbf80ead5251d01fc380482c38126decf0
silva-angelo <silva.angelo@hotmail.com>
2021-06-23 15:12:44 +0100

    Made the method static and deleted properties in the class

--- src/academy/mindswap/training/Training.java
+++ src/academy/mindswap/training/Training.java
@@ -4,29 +4,27 @@ import academy.mindswap.players.Players;
 
 
 public class Training {
-    Players player;
-    Players age;
 
+    public static void developPlayers(Players player) {
 
-    public void developPlayers(Players player) {
-
-        System.out.println("Player x, picked " + player.getName() + "for a training session");
+        System.out.println("Player x, picked " + player.getName() + " for a training session");
         int playerDevelopment = 0;
 
 
         if (player.getAge() <= 21) {
             playerDevelopment = (int) (Math.random() * 5);
+            System.out.println("playerDevelopment = " + playerDevelopment);
             player.setOverall(player.getOverall() + playerDevelopment);
-            System.out.println(player.getName() + "improve is overall to " + player.getOverall());
+            System.out.println(player.getName() + " improve is overall to " + player.getOverall());
         } else if (player.getAge() > 21 && player.getAge() <= 25) {
             playerDevelopment = (int) (Math.random() * 3);
             player.setOverall(player.getOverall() + playerDevelopment);
-            System.out.println(player.getName() + "improve is overall to " + player.getOverall());
+            System.out.println(player.getName() + " improve is overall to " + player.getOverall());
 
         } else {
             playerDevelopment = (int) (Math.random() * 1);
-            player.setOverall(playerDevelopment);
-            System.out.println(player.getName() + "improve is overall to " + player.getOverall());
+            player.setOverall(player.getOverall() + playerDevelopment);
+            System.out.println(player.getName() + " improve is overall to " + player.getOverall());
 
 
         }
