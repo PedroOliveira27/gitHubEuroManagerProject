bbf53a4b9050c055bf9f1b49117d43c74d7724dd
silva-angelo <silva.angelo@hotmail.com>
2021-06-24 10:53:50 +0100

    Changes related to the name refactor of the class Match

--- src/Game/Match.java
+++ src/Game/Match.java
@@ -2,11 +2,11 @@ package Game;
 
 import academy.mindswap.teams.Team;
 
-public class GameMatch {
+public class Match {
     private Team team1;
     private Team team2;
 
-    public GameMatch(Team team1, Team team2) {
+    public Match(Team team1, Team team2) {
         this.team1 = team1;
         this.team2 = team2;
     }
