22f152787de68d1faced72b42cc32c3797f920ff
PedroOliveira27 <pedrofg.oliveira@hotmail.com>
2021-06-25 11:12:02 +0100

    Simplified code

--- src/academy/mindswap/Game/Match.java
+++ src/academy/mindswap/Game/Match.java
@@ -2,12 +2,16 @@ package academy.mindswap.Game;
 
 import academy.mindswap.teams.Team;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Match {
     private Team team1;
     private Team team2;
     private int team1Score;
     private int team2Score;
-
+    private List<Team> winTeamList = new LinkedList<>();
+    private List<Team> loseTeamList = new LinkedList<>();
 
     public Match(Team team1, Team team2) {
         this.team1 = team1;
@@ -15,14 +19,27 @@ public class Match {
     }
 
 
-    public Team play() {
+    public Team start() {
         System.out.println("Match day!!!Welcome to the stadium!Today\n " +
                 team1.getName() + " is playing against " + team2.getName());
-         team1Score = 0;
-         team2Score = 0;
+        team1Score = 0;
+        team2Score = 0;
         double matchTime = 0;
 
-        while (matchTime < 90) {
+        Team teamWinner = play(90);
+
+        if (teamWinner == null) {
+            teamWinner = play(30);
+        }
+        System.out.println("The game has ended!!The final score is \n" +
+                "[" + team1.getName() + " " + team1Score + "]" + " - " +
+                "[" + team2.getName() + " " + team2Score + "]");
+        return teamWinner;
+    }
+
+    private Team play(double maxMatchTime) {
+    double currentmatchTime = 0;
+        while (currentmatchTime < maxMatchTime) {
             Team team = aGoalIsScored();
             if (team == team1)
                 team1Score++;
@@ -30,28 +47,12 @@ public class Match {
                 team2Score++;
             System.out.println("[" + team1.getName() + " " + team1Score + "]" + " - " +
                     "[" + team2.getName() + " " + team2Score + "]");
-            matchTime += 22.5;
-
-            if (matchTime == 45) {
-                System.out.println("It`s half-time.Its time to give the players a motivational speech!.Lets start the second half");
+            if (maxMatchTime/2 ==currentmatchTime){
+                System.out.println("It's half time! Are you ready ? Let's go TEAM!");
             }
-
-            if (team1Score > team2Score) {
-                System.out.println(team1.getName() + " has won the game");
-
-            }
-
-
+            currentmatchTime += (maxMatchTime / 4);
         }
-        Team winner = checkWhoWon();
-
-
-        System.out.println("The game has ended!!The final score is \n" +
-                "[" + team1.getName() + " " + team1Score + "]" + " - " +
-                "[" + team2.getName() + " " + team2Score + "]");
-
-//        System.out.println("The starting eleven for " + team1.getName() + " is ");
-        return winner;
+        return checkWhoWon();
     }
 
     private Team aGoalIsScored() {
@@ -82,7 +83,6 @@ public class Match {
         System.out.println(team.getName() + " has a great chance to score");
 
         int chanceToScore = (team.getOverall() * 100) / totalOverall;
-        System.out.println(chanceToScore);
         if (chanceToScore <= (int) (Math.random() * 100)) {
             System.out.println(team.getName() + " Score a gooooooooooooaaaaaal!!!");
             return true;
@@ -98,20 +98,20 @@ public class Match {
 
         if (team1Score > team2Score) {
             System.out.println(team1.getName() + " has won the game");
-
+            winTeamList.add(team1);
+            loseTeamList.add(team2);
             return team1;
 
         } else if (team1Score < team2Score) {
             System.out.println(team2.getName() + " has won the game");
-
+            winTeamList.add(team2);
+            loseTeamList.add(team1);
             return team2;
 
         } else {
             System.out.println("At the 90 minutes mark the games is tied");
             return null;
         }
-
-
     }
 }
 
