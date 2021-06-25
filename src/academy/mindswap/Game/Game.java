eccac3e242eaa6e40e581254a6811492f237e4f3
PedroOliveira27 <pedrofg.oliveira@hotmail.com>
2021-06-25 09:28:33 +0100

    Added randomGame Method

--- src/academy/mindswap/Game/Game.java
+++ src/academy/mindswap/Game/Game.java
@@ -7,7 +7,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    Groups group;
+
+    public Game() {
+       group = new Groups();
+    }
+    public Game(Groups group) {
+        this.group = group;
+    }
 
 
+    public void RandomGame (){
+        int max=3;
+        int min=0;
+        int randomFirstTeamPlay = (int) (Math.random() * (max-min+1))+min;
+        System.out.println(randomFirstTeamPlay);
+        int randomSecontTeamPlay = (int) (Math.random() * (max-min+1))+min;
+        System.out.println(randomSecontTeamPlay);
+        if (randomFirstTeamPlay != randomSecontTeamPlay){
+            System.out.println(group.createGroup.get(randomFirstTeamPlay).getName());
+            System.out.println(group.createGroup.get(randomSecontTeamPlay).getName());
+        }
+
+
+    }
 
 }
