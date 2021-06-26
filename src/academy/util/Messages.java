f76d325443e81acb96fa43fa2fa4ae6f23419b7a
DavidCortT <davidcorteztrind@gmail.com>
2021-06-26 17:06:12 +0100

    create a util class Messages

---
+++ src/academy/util/Messages.java
@@ -0,0 +1,15 @@
+package academy.util;
+
+public class Messages {
+    public static final String START_GAME = "Match day!!!Welcome to the stadium!Today %s is playing against %s\n ";
+    public static final String PLAYER_SCORE = "";
+    public static final String TEAM_WIN = "";
+    public static final String PLAYER_TRAINING = "";
+    public static final String PENALTIES ="";
+
+
+
+
+
+
+}
